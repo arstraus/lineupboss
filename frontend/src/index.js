8b2a5a81c9762c5af4ca25c6e7bf411890917a62
arstraus <arstraus@gmail.com>
2025-03-22 13:52:12 -0500

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import { AuthProvider } from "./services/AuthContext";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./index.css";
 
